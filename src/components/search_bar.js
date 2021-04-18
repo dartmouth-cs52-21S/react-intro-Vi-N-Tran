762c09dbc3123a06cc2cc8718cd9c656e2a4eac5
Vi Tran <Vi.n.tran.23@dartmouth.edu>
2021-04-18 15:58:12 -0700

    end of debugging in sa

---
+++ src/components/search_bar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchterm: '' };
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  onInputChange = (event) => {
+    this.setState({ searchterm: event.target.value });
+    this.props.onSearchChange(event.target.value);
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div id="search-bar">
+        <input onChange={this.onInputChange} value={this.state.searchterm} />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
